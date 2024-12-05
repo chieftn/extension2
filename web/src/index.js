678dab95c35eb33805a967eaf67d65796f9d3650
Rick <rkessler@microsoft.com>
2024-12-04 20:46:15 -0800

    initial react view.

---
+++ web/src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("react-dom/client");
+const domNode = document.getElementById('root');
+const root = (0, client_1.createRoot)(domNode);
+root.render(<div>Hello world</div>);
+//# sourceMappingURL=index.js.map
