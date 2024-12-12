cd2a7d9a4afb02b16c37f6771849b8cb5444be69
Rick <rkessler@microsoft.com>
2024-12-12 08:56:43 -0800

    fix styling.

--- web/src/views/graph/sections/graph/components/canvasNode.tsx
+++ web/src/views/graph/sections/graph/components/canvasNode.tsx
@@ -8,27 +8,36 @@ type OperatorNode = Node<Operator>;
 
 const useCanvasNodeStyles = makeStyles({
     rootStyle: {
-        display: 'flex',
-        width: `${nodeWidth}px`,
-        height: `${nodeHeight}px`,
         backgroundColor: tokens.colorNeutralBackground1,
-        boxShadow: tokens.shadow4,
         border: `2px solid ${tokens.colorNeutralStroke1}`,
         borderRadius: tokens.borderRadiusMedium,
+        boxShadow: tokens.shadow4,
+        height: `${nodeHeight}px`,
+        overflow: 'hidden',
+        width: `${nodeWidth}px`,
     },
-    internalStyle: {
+    bodyStyle: {
+        display: 'flex',
+        flexDirection: 'column',
+        height: '100%',
+        justifyContent: 'center',
+    },
+    nameStyle: {
         overflow: 'hidden',
         textOverflow: 'ellipsis',
+        paddingInline: tokens.spacingHorizontalS,
     },
 });
 
 export const CanvasNode: React.FC<NodeProps<OperatorNode>> = ({ data }) => {
-    const { rootStyle, internalStyle } = useCanvasNodeStyles();
+    const { rootStyle, bodyStyle, nameStyle } = useCanvasNodeStyles();
     return (
         <div className={rootStyle}>
             <Handle type="target" position={Position.Top} />
-            <div className={internalStyle}>{data.id}</div>
-            <Handle type="source" position={Position.Bottom} id="a" />
+            <div className={bodyStyle}>
+                <div className={nameStyle}>{data.id}</div>
+            </div>
+            <Handle type="source" position={Position.Bottom} />
         </div>
     );
 };
